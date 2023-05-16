82158ae08952271f6e8912f4712eaf0efcf774c9
Leroy Lau <llau65@my.bcit.ca>
2023-05-16 00:43:55 -0700

    Working on power-up logic

--- index.js
+++ index.js
@@ -81,30 +81,30 @@ const setup = () => {
     // Initial variable declaration
     var firstCard = undefined;
     var secondCard = undefined;
-    var totalPairs = 3; // Total number of pairs
+    var totalPairs = 0; // Total number of pairs
     var matches = 0; // Number of matched pairs
     var clicks = 0; // Number of clicks
     var matchesLeft = totalPairs; // Number of matches remaining
-    var timer = 100; // Timer for the game
+    var timer = 0; // Timer for the game
     var time = 0;
 
     // New values assigned to variables based on difficulty selected
-    if (difficulty === "easy"){
-      totalPairs = 3; 
-      timer = 100; 
-
-    } else if (difficulty === "medium") {
+if (difficulty === "medium") {
       totalPairs = 6;
       timer = 200; 
       matchesLeft = totalPairs;
       $("#game_grid").css("width", "800px");
       $("#game_grid").css("height", "600px");
-    } else {
+    } else if (difficulty === "hard") {
       totalPairs = 12;
       matchesLeft = totalPairs;
       timer = 300;
       $("#game_grid").css("width", "1200px");
       $("#game_grid").css("height", "800px");
+    } else {
+      timer = 100;
+      totalPairs = 3;
+      matchesLeft = totalPairs;
     }
     
     // Adds in value of the variables to their respective HTML 
@@ -119,8 +119,8 @@ const setup = () => {
     time++;
 
     var matchTimer = setInterval(() => {
-      // Stops the timer if time is up
-      if (time === timer) {
+      // Stops the timer if time is up, subtracted 1 from time to properly display time on HTML
+      if (time-1 === timer) {
         clearInterval(matchTimer);
         $("header").text("Time's up!");
         $("#game_grid").html("<a href='./index.html'> Try again! </a>");
@@ -129,6 +129,19 @@ const setup = () => {
       $("#time").text(time++);
     }, 1000);
 
+    const powerUp = function () {
+    setTimeout(() => {
+      alert("Power up!");
+      $(".pokeCard").not(".disabled").toggleClass("flip");
+      setTimeout(() => {
+        $(".pokeCard").not(".disabled").toggleClass("flip");
+      }, 1500);
+    });
+      var randomInterval = Math.random() * 30000;
+      setInterval(powerUp, randomInterval);
+  }
+
+
     // Function call to generate cards and attach event listeners
     getRandomPokemon(totalPairs).then(() => {
       $(".pokeCard").on("click", function () {
@@ -136,6 +149,7 @@ const setup = () => {
         if (!firstCard) {
           firstCard = $(this).find(".front_face")[0];
           $(this).toggleClass("flip");
+          $(this).toggleClass("disabled");
 
           // Increments clicks on click and updates value shown in HTML
           clicks++;
@@ -148,6 +162,7 @@ const setup = () => {
           if (!secondCard) {
             secondCard = $(this).find(".front_face")[0];
             $(this).toggleClass("flip");
+            $(this).toggleClass("disabled");
             clicks++;
             $("#clicks").text(clicks);
             console.log(clicks);
@@ -157,7 +172,6 @@ const setup = () => {
           }
 
           if (firstCard.src == secondCard.src) {
-
             matches++;
             $("#matches").text(matches);
             $("#left").text(matchesLeft - matches);
@@ -171,26 +185,28 @@ const setup = () => {
             secondCard = undefined;
 
           } else {
-
             // if not a match, flip cards back over after a 1 second delay
             setTimeout(() => {
               $(`#${firstCard.id}`).parent().toggleClass("flip");
+              $(`#${firstCard.id}`).parent().toggleClass("disabled");
               $(`#${secondCard.id}`).parent().toggleClass("flip");
+              $(`#${secondCard.id}`).parent().toggleClass("disabled");
               firstCard = undefined;
               secondCard = undefined;
             }, 1000);
           }
         }
-
         // Stops timer and ends the game once the user has matched all the pairs
         if (matches == totalPairs) {
           setTimeout(() => {
             alert("You win!");
           }, 500);
           clearInterval(matchTimer);
+          clearInterval(powerUp);
         }
       });
     });
+   
   }
 
   // Adds event listener to start button, calls startGame function.
