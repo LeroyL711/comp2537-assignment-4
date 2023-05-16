e6849f4ea99714f86e076abcc1cb1e6c79a3d68f
Leroy Lau <llau65@my.bcit.ca>
2023-05-16 00:08:33 -0700

    Added in a bunch of conditional statements to determine how many cards to generate based on difficulty, and added in API call for random pokemon from PokeAPI

--- index.js
+++ index.js
@@ -1,110 +1,202 @@
+// Generates random pairs of pokemon
+async function getRandomPokemon(numPokemonPairs) {
+  const randomPokemon = [];
+  for (i = 0; i < numPokemonPairs; i++) {
 
-const setup = () => {
-  let firstCard = undefined
-  let secondCard = undefined
-  var totalPairs = 3; // Total number of pairs
-  var matches = 0; // Number of matched pairs
-  var clicks = 0; // Number of clicks
-  var timer = 0; // Timer for the game
+    // Math.random function call to get a random number between 1 and 810
+    var randomPokemonID = Math.floor((Math.random() * 810)) + 1;
 
-  
-  $(".card").on(("click"), function () {
-    $(this).toggleClass("flip");
-
-    if (!firstCard) {
-      firstCard = $(this).find(".front_face")[0];
-      console.log(firstCard)
+    // Checks if the randomPokemon array already includes the randomPokemonID
+    // If it does, generate another random number
+    if (randomPokemon.includes(randomPokemonID)){
+      i--;
     } else {
-      secondCard = $(this).find(".front_face")[0];
-      $(this).addClass("disabled");
-      console.log(firstCard, secondCard);
-      if (
-        firstCard.src
-        ==
-        secondCard.src
-      ) {
-        console.log("match")
-        $(`#${firstCard.id}`).parent().off("click");
-        $(`#${secondCard.id}`).parent().off("click");
-        firstCard = undefined;
-        secondCard = undefined;
-        console.log("First card: " + firstCard)
-        console.log("Second card: " + secondCard)
-      
-      } else {
-        console.log("no match")
-        console.log("First card: " + firstCard)
-
-        console.log("Second card: " + secondCard)
-        setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip")
-          $(`#${secondCard.id}`).parent().toggleClass("flip")
-          firstCard = undefined;
-          secondCard = undefined;
-        }, 1000)
-      }
+    // Add a pair of matching pokemon if ID does not already exist in array
+    randomPokemon.push(randomPokemonID);
+    randomPokemon.push(randomPokemonID);
     }
+  }
 
-  });
+  // Shuffle array
+  randomPokemon.sort(() => Math.random() - 0.5);
+
+  // Iterates through the randomPokemon array
+  for (i = 0; i < randomPokemon.length; i++){
+    const pokemonID = randomPokemon[i];
+
+    // retrieves image for respective pokemon ID and generates a card for that pokemon
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonID}`
+    );
+    $("#game_grid").append(`<div class="pokeCard">
+    <img id="img${i}" class="front_face" src=${res.data.sprites.other["official-artwork"].front_default} alt="">
+    <img class="back_face" src="back.webp" alt="">
+  </div>`);
+    }
+  
 }
 
+const setup = () => {
+
+  // Difficulty level
+  var difficulty = "";
+  
+
+  // On-click even listeners attached to difficulty options
+  $("#option1").click(function () {
+    difficulty = $("#option1").val();
+    console.log(difficulty)
+  })
+  
+  $("#option2").click(function () {
+    difficulty = $("#option2").val();
+    console.log(difficulty)
+  })
+
+  $("#option3").click(function () {
+    difficulty = $("#option3").val();
+    console.log(difficulty)
+  })
+
+
+  // Function to initialize the game 
+  function startGame() {
+
+
+    // Buttons to allow user to switch between light and dark mode
+    $("#dark").click(function () {
+    $("#game_grid").css("background-color", "black");
+    });
+    $("#light").click(function () {
+    $("#game_grid").css("background-color", "white");
+    });
+
+    //Upon starting game, make game grid, info, and theme buttons visible
+    $("#start").css("display", "none");
+    $("#game_grid").css("display", "");
+    $("#info").css("display", "");
+    $("#themes").css("display", "");
+
+
+    // Initial variable declaration
+    var firstCard = undefined;
+    var secondCard = undefined;
+    var totalPairs = 3; // Total number of pairs
+    var matches = 0; // Number of matched pairs
+    var clicks = 0; // Number of clicks
+    var matchesLeft = totalPairs; // Number of matches remaining
+    var timer = 100; // Timer for the game
+    var time = 0;
+
+    // New values assigned to variables based on difficulty selected
+    if (difficulty === "easy"){
+      totalPairs = 3; 
+      timer = 100; 
+
+    } else if (difficulty === "medium") {
+      totalPairs = 6;
+      timer = 200; 
+      matchesLeft = totalPairs;
+      $("#game_grid").css("width", "800px");
+      $("#game_grid").css("height", "600px");
+    } else {
+      totalPairs = 12;
+      matchesLeft = totalPairs;
+      timer = 300;
+      $("#game_grid").css("width", "1200px");
+      $("#game_grid").css("height", "800px");
+    }
+    
+    // Adds in value of the variables to their respective HTML 
+    $("#clicks").text(clicks);
+    $("#total").text(totalPairs);
+    $("#matches").text(matches);
+    $("#left").text(matchesLeft - matches);
+    $("#timer").text(timer);
+    $("#time").text(time);
+
+    // Increments time by 1 - setInterval would otherwise start counting from 0 again
+    time++;
+
+    var matchTimer = setInterval(() => {
+      // Stops the timer if time is up
+      if (time === timer) {
+        clearInterval(matchTimer);
+        $("header").text("Time's up!");
+        $("#game_grid").html("<a href='./index.html'> Try again! </a>");
+        return;
+      }
+      $("#time").text(time++);
+    }, 1000);
+
+    // Function call to generate cards and attach event listeners
+    getRandomPokemon(totalPairs).then(() => {
+      $(".pokeCard").on("click", function () {
+        
+        if (!firstCard) {
+          firstCard = $(this).find(".front_face")[0];
+          $(this).toggleClass("flip");
+
+          // Increments clicks on click and updates value shown in HTML
+          clicks++;
+          $("#clicks").text(clicks);
+        } else {
+          // If same card is clicked twice, nothing happens
+          if ($(this).find(".front_face")[0] === firstCard) {
+            return;
+          }
+          if (!secondCard) {
+            secondCard = $(this).find(".front_face")[0];
+            $(this).toggleClass("flip");
+            clicks++;
+            $("#clicks").text(clicks);
+            console.log(clicks);
+          } else {
+            // If two cards are already clicked, do nothing
+            return;
+          }
+
+          if (firstCard.src == secondCard.src) {
+
+            matches++;
+            $("#matches").text(matches);
+            $("#left").text(matchesLeft - matches);
+
+            // Turns off event listeners if matched
+            $(`#${firstCard.id}`).parent().off("click");
+            $(`#${secondCard.id}`).parent().off("click");
+
+            //Resets first and secondCard variables
+            firstCard = undefined;
+            secondCard = undefined;
+
+          } else {
+
+            // if not a match, flip cards back over after a 1 second delay
+            setTimeout(() => {
+              $(`#${firstCard.id}`).parent().toggleClass("flip");
+              $(`#${secondCard.id}`).parent().toggleClass("flip");
+              firstCard = undefined;
+              secondCard = undefined;
+            }, 1000);
+          }
+        }
+
+        // Stops timer and ends the game once the user has matched all the pairs
+        if (matches == totalPairs) {
+          setTimeout(() => {
+            alert("You win!");
+          }, 500);
+          clearInterval(matchTimer);
+        }
+      });
+    });
+  }
+
+  // Adds event listener to start button, calls startGame function.
+  $("#start").click(function () {
+    startGame();
+  });
+};
+
 $(document).ready(setup);
-
-
-
-  // Function to start the game
-//   function startGame() {
-//     // Reset game state
-//     matches = 0;
-//     clicks = 0;
-//     timer = 0;
-//     updateGameInfo();
-
-//         // Compare the cards
-//         if (card1 === card2) {
-//           // Cards are a match
-//           $('.selected').addClass('matched').removeClass('selected');
-//           matches++;
-
-//           // Check if all pairs are matched
-//           if (matches === totalPairs) {
-//             // Game over
-//             clearInterval(timerInterval);
-//             alert('Congratulations! You won the game.');
-//           }
-//         } else {
-//           // Cards are not a match
-//           setTimeout(function() {
-//             $('.selected').find('.back_face').show();
-//             $('.selected').find('.front_face').hide();
-//             $('.selected').removeClass('selected');
-//           }, 1000);
-//         }
-
-//         // Update game information
-//         updateGameInfo();
-//       }
-//     });
-
-//     // Start the timer
-//     var timerInterval = setInterval(function() {
-//       timer++;
-//       updateGameInfo();
-//     }, 1000);
-//   }
-
-//   // Function to update game information
-//   function updateGameInfo() {
-//     $('#total').text(totalPairs);
-//     $('#matches').text(matches);
-//     $('#left').text(totalPairs - matches);
-//     $('#clicks').text(clicks);
-//     $('#timer').text(timer);
-//     $('#time').text(timer === 1 ? '' : 's');
-//   }
-
-//   // Bind click event to the "Start!" button
-//   $('#start').click(function() {
-//     startGame();
-//   });
-// });
